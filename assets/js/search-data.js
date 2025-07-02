1b9d8e2a08e5913807ac97e07bbeba979c1b91aa
Aufinal <ludo_529@hotmail.com>
2025-07-02 15:29:34 +0000

    Deploying to gh-pages from @ Aufinal/aufinal.github.io@12e6856806e72452e6e031ec365dd1eab3c8c131 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,86 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-i-will-be-in-corsica-between-aug-1-12-for-the-statistical-physics-and-machine-learning-school",
+          title: 'I will be in Corsica between Aug 1-12, for the Statistical physics and...',
+          description: "",
+          section: "News",},{id: "news-starting-sep-1st-i-ll-start-a-position-as-assistant-professor-at-ensai-in-rennes-goodbye-switzerland-and-hello-brittany",
+          title: 'Starting Sep. 1st, I’ll start a position as Assistant Professor at ENSAI, in...',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%6C%75%64%6F%76%69%63.%73%74%65%70%68%61%6E@%65%6E%73%61%69.%66%72", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Aufinal", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=mEd3WCsAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-5612-3577", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
